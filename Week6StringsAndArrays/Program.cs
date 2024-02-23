71802a1dd12a0a69a33189c3ea9065b24f16f95a
KristjanKaa <159649951+KristjanKaa@users.noreply.github.com>
2024-02-23 18:15:09 +0200

    Added mu project

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+string hello = " we are the champions?";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+ firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+//hello[0] = 'H';
+
+Console.WriteLine(firstLetter);
+
+//int wordCounter = 1;
+
+//for(int i = 0; i < hello.Length; i++)
+{
+    //Console.WriteLine($"{i} symbol in string: {hello[i]}");
+    //if (hello[i] == ' ')
+{
+        //wordCounter++;
+       // if (char.IsWhiteSpace(hello[i]))
+       // { wordCounter++; }
+
+}
+}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter}");
